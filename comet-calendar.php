ff9f2604e6c4cbfba40b45a18e2376feae4cb8b9
Wun Chiou <wun@utdallas.edu>
2017-10-31 10:08:10 -0500

    initial commit

---
+++ comet-calendar.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * The plugin bootstrap file
+ *
+ * This file is read by WordPress to generate the plugin information in the plugin
+ * admin area.
+ * 
+ * This file also includes all of the dependencies used by the plugin,
+ * registers the activation and deactivation functions, and defines a function
+ * that starts the plugin.
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Comet Calendar
+ * Plugin URI:        https://github.com/wunc/wordpress-plugin-presentation
+ * Description:       Show a comet calendar feed via a shortcode.
+ * Version:           1.0.0
+ * Author:            Wun Chiou
+ * Author URI:        https://www.wunchiou.com
+ * License:           MIT
+ * License URI:       http://opensource.org/licenses/MIT
+ * Text Domain:       comet-calendar
+ * Domain Path:       /languages
+ */
+
+echo 'Hello WordPress Plugin World!';
+?>
---
+++ readme.md
@@ -0,0 +1,11 @@
+# WordPress Plugin Presentation
+
+How to create a WordPress plugin from scratch!
+
+## Introduction
+
+...
+
+## How to follow along
+
+The branches in this repository represents the steps in the presentation. To load the code for step 1, just `git checkout step1`, and so forth for other steps. To get to the final version, just checkout the master branch.
